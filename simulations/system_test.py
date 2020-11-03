e85582c9c0dc5cf485a5480b212545037707d2f5
Piece123 <taka.rock.i4@gmail.com>
2020-11-03 21:38:24 +0900

    システムを調整

--- simulations/system_test.py
+++ simulations/system_test.py
@@ -1,34 +1,34 @@
 from src import modules as m
 from src.system_model import SystemModel
-import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     params = {
         'n': 2 * 10 ** 4,  # サンプルのn数
         'gamma': 0.3,
         'phi': 3.0,
-        'PA_IBO_dB': 3,
+        'PA_IBO_dB': 2,
         'PA_rho': 2,
 
-        'LNA_IBO_dB': 3,
+        'LNA_IBO_dB': 2,
         'LNA_rho': 2,
-
-        'SNR_MIN': 0,
-        'SNR_MAX': 12,
-        'sur_num': 12,
     }
 
-    snrs_db = np.linspace(params['SNR_MIN'], params['SNR_MAX'], params['sur_num'])
-    sigmas = m.sigmas(snrs_db)  # SNR(dB)を元に雑音電力を導出
+    system_model = SystemModel(
+        params['n'],
+        0,
+        params['gamma'],
+        params['phi'],
+        params['PA_IBO_dB'],
+        params['PA_rho'],
+        params['LNA_IBO_dB'],
+        params['LNA_rho'],
+    )
 
-    for index, sigma in enumerate(sigmas):
-        system_model = SystemModel(
-            params['n'],
-            sigma,
-            params['gamma'],
-            params['phi'],
-            params['PA_IBO_dB'],
-            params['PA_rho'],
-            params['LNA_IBO_dB'],
-            params['LNA_rho'],
-        )
+    plots = 10
+    plt.scatter(system_model.x[::plots].real, system_model.x[::plots].imag, color="black", label="x")
+    plt.scatter(system_model.x_iq[::plots].real, system_model.x_iq[::plots].imag, color="blue", label="x_iq")
+    plt.scatter(system_model.x_pa[::plots].real, system_model.x_pa[::plots].imag, color="red", label="x_pa")
+    plt.legend()
+    plt.show()
+    print("end")
