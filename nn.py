f5c77bde6c4cbac24d1bacdb6a6437e8b078a58b
Piece123 <taka.rock.i4@gmail.com>
2020-11-03 14:06:28 +0900

    パラメータを記録できるように

--- nn.py
+++ nn.py
@@ -9,48 +9,64 @@ import matplotlib.pyplot as plt
 import os
 import datetime
 
+# シミュレーション結果の保存先を作成する
+dt_now = datetime.datetime.now()
+dirname = 'results/' + dt_now.strftime("%Y/%m/%d/%H_%M_%S")
+os.makedirs(dirname, exist_ok=True)
+result_txt = open(dirname+'/result.txt', mode='w')
+print('start', file=result_txt)
+print(dt_now.strftime("%Y/%m/%d %H:%M:%S"), file=result_txt)
+
 # グラフ
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 22
 plt.rcParams["xtick.direction"] = "in"
 plt.rcParams["ytick.direction"] = "in"
-dt_now = datetime.datetime.now()
-dirname = dt_now.strftime("%Y/%m/%d/%H_%M_%S")
-os.makedirs('results/'+dirname, exist_ok=True)
 
 # パラメータ
-n = 2 * 10 ** 4  # サンプルのn数
+params = {
+    'n': 2 * 10 ** 4,  # サンプルのn数
+    'gamma': 0.3,
+    'phi': 3.0,
+    'PA_IBO_dB': 3,
+    'PA_rho': 2,
 
-gamma = 0.3
-phi = 3.0
+    'LNA_IBO_dB': 3,
+    'LNA_rho': 2,
 
-PA_IBO_dB = 3
-PA_rho = 2
+    'SNR_MAX': 0,
+    'SNR_MIN': 0,
+    'sur_num': 1,
 
-LNA_IBO_dB = 3
-LNA_rho = 2
-
-SNR_MAX = 0
-SNR_MIN = 0
-sur_num = 1
-
-nHidden = 5
-nEpochs = 20
-learningRate = 0.004
-trainingRatio = 0.9  # 全体のデータ数に対するトレーニングデータの割合
-batchSize = 32
+    'nHidden': 5,
+    'nEpochs': 20,
+    'learningRate': 0.004,
+    'trainingRatio': 0.9,  # 全体のデータ数に対するトレーニングデータの割合
+    'batchSize': 32
+}
+print('params', file=result_txt)
+print(params, file=result_txt)
 
 # データを生成する
-snrs_db = np.linspace(SNR_MIN, SNR_MAX, sur_num)
+snrs_db = np.linspace(params['SNR_MIN'], params['SNR_MAX'], params['sur_num'])
 sigmas = m.sigmas(snrs_db)  # SNR(dB)を元に雑音電力を導出
 
-bers = np.zeros(sur_num)
+bers = np.zeros(params['sur_num'])
 for index, sigma in enumerate(sigmas):
-    system_model = SystemModel(n, sigma, gamma, phi, PA_IBO_dB, PA_rho, LNA_IBO_dB, LNA_rho)
+    system_model = SystemModel(
+        params['n'],
+        sigma,
+        params['gamma'],
+        params['phi'],
+        params['PA_IBO_dB'],
+        params['PA_rho'],
+        params['LNA_IBO_dB'],
+        params['LNA_rho']
+    )
 
     # NNを生成
     input = Input(shape=(4,))
-    hidden1 = Dense(nHidden, activation='relu')(input)
+    hidden1 = Dense(params['nHidden'], activation='relu')(input)
     output1 = Dense(1, activation='linear')(hidden1)
     output2 = Dense(1, activation='linear')(hidden1)
     model = Model(inputs=input, outputs=[output1, output2])
@@ -59,7 +75,7 @@ for index, sigma in enumerate(sigmas):
     model.compile(RAdam(), loss='mse')
 
     # トレーニングデータの生成
-    trainingSamples = int(np.floor(system_model.x.size * trainingRatio))
+    trainingSamples = int(np.floor(system_model.x.size * params['trainingRatio']))
     x_train = system_model.x[0:trainingSamples]
     y_train = system_model.y[0:trainingSamples]
     s_train = system_model.s[0:trainingSamples]
@@ -84,7 +100,7 @@ for index, sigma in enumerate(sigmas):
     test[:, 3] = y_test.imag
 
     # 学習
-    history = model.fit(train, [s_train.real, s_train.imag], epochs=nEpochs, batch_size=batchSize, verbose=2,
+    history = model.fit(train, [s_train.real, s_train.imag], epochs=params['nEpochs'], batch_size=params['batchSize'], verbose=2,
                         validation_data=(test, [s_test.real, s_test.imag]))
 
     # 学習したモデルを評価
@@ -109,11 +125,10 @@ for index, sigma in enumerate(sigmas):
     plt.xlabel('Training Epoch')
     plt.legend(['Training Frame', 'Test Frame'], loc='lower right')
     plt.grid(which='major', alpha=0.25)
-    plt.xlim([0, nEpochs + 1])
-    plt.xticks(range(1, nEpochs, 2))
-    plt.savefig('results/'+dirname+'/sigma_'+str(sigma)+'_NNconv.pdf')
+    plt.xlim([0, params['nEpochs'] + 1])
+    plt.xticks(range(1, params['nEpochs'], 2))
+    plt.savefig(dirname + '/sigma_' + str(sigma) + '_NNconv.pdf')
     plt.show()
-
 
 # SNR-BERグラフ
 fig = plt.figure(figsize=(8, 6))
@@ -121,7 +136,7 @@ ax = fig.add_subplot(111)
 ax.set_xlabel(r"SNR (dB)")
 ax.set_ylabel(r"$BER$")
 ax.set_yscale('log')
-ax.set_xlim(SNR_MIN, SNR_MAX)
+ax.set_xlim(params['SNR_MIN'], params['SNR_MAX'])
 y_min = pow(10, 0)
 y_max = pow(10, -6)
 ax.set_ylim(y_max, y_min)
@@ -130,7 +145,8 @@ ax.legend()
 
 ax.scatter(snrs_db, bers, color="blue")
 
-plt.savefig('results/'+dirname+'/SNR_BER.pdf')
+plt.savefig(dirname + '/SNR_BER.pdf')
 plt.show()
 
-print("end")
+print("end",file=result_txt)
+result_txt.close()
