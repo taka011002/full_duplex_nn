e3a6b6f736b25a1b7b1ecdb17e4913ba1cfbc997
Piece123 <taka.rock.i4@gmail.com>
2020-11-30 15:36:39 +0900

    varのチャネルの振り方を逆に

--- simulations/non_cancell_var_ber_chain_load.py
+++ simulations/non_cancell_var_ber_chain_load.py
@@ -65,22 +65,23 @@ if __name__ == '__main__':
     # データを生成する
     snr = params['snr']
     sigma = m.sigmas(snr)  # SNR(dB)を元に雑音電力を導出
+    params['var_min'] = -100
     var_si_var_s_db = np.linspace(params['var_min'], params['var_max'], params['var_num'])
     var_si_var_s = m.to_exact_number(var_si_var_s_db)
-    var_si = 1
-    var_s_list = var_si / var_si_var_s
+    var_s = 1
+    var_si_list = var_si_var_s * var_s
     non_cancell_error = np.zeros((params['var_num'], params['number_of_trials']))
     for trials_index in tqdm(range(params['number_of_trials'])):
-        for var_s_index, var_s in enumerate(var_s_list):
+        for var_si_index, var_si in enumerate(var_si_list):
             # 通信路は毎回生成する
             h_si = m.channel(1, params['h_si_len'], var_si)
-            h_s = m.channel(1, params['h_s_len'], var_s)
+            h_s = m.channel(1, params['h_s_len'], var_si)
             logging.info('random channel')
             logging.info('h_si:{0.real}+{0.imag}i'.format(h_si))
             logging.info('h_s:{0.real}+{0.imag}i'.format(h_s))
 
             logging.info("number_of_trials_index:" + str(trials_index))
-            logging.info("var_s_index:" + str(var_s_index))
+            logging.info("var_s_index:" + str(var_si_index))
             system_model = SystemModel(
                 params['n'],
                 sigma,
@@ -88,8 +89,8 @@ if __name__ == '__main__':
                 params['phi'],
                 params['PA_IBO_db'],
                 params['PA_rho'],
-                params['LNA_IBO_dB'],
-                params['LNA_rho'],
+                1000,
+                1000,
                 h_si,
                 h_s,
                 params['h_si_len'],
@@ -98,7 +99,7 @@ if __name__ == '__main__':
 
             demodulate = m.demodulate_qpsk(system_model.y.reshape(system_model.y.shape[0]))
             error = np.sum(system_model.d_s != demodulate)
-            non_cancell_error[var_s_index][trials_index] = error
+            non_cancell_error[var_si_index][trials_index] = error
 
     # SNR-BERグラフ
     fig = plt.figure(figsize=(8, 6))
@@ -110,7 +111,7 @@ if __name__ == '__main__':
     y_max = pow(10, -6)
     ax.set_ylim(y_max, y_min)
     # ax.set_xlim(params['var_min'], params['var_max'])
-    ax.set_xlim(-2.5, params['var_max'])
+    ax.set_xlim(params['var_min'], params['var_max'])
     ax.grid(linestyle='--')
 
     train_data = params['n'] - (params['n'] * params['trainingRatio'])
@@ -122,27 +123,27 @@ if __name__ == '__main__':
     non_cancell_error_sum = np.sum(non_cancell_error, axis=1)
     non_cancell_error = non_cancell_error_sum / (params['n'] * params['number_of_trials'])
     ax.plot(var_si_var_s_db, non_cancell_error, color='k', marker='o', linestyle='--', label="Non cancelled")
-    ax.plot(var_si_var_s_db, bers, color='r', marker='o', linestyle='--', label="NN cancelled")
+    # ax.plot(var_si_var_s_db, bers, color='r', marker='o', linestyle='--', label="NN cancelled")
 
     ax.legend()
     plt.savefig(dirname + '/var_BER.pdf', bbox_inches='tight')
 
     # Plot learning curve
-    for var_s_index, var_s in enumerate(var_si_var_s_db):
-        plt.figure()
-
-        loss_avg = np.mean(losss[var_s_index], axis=0).T
-        val_loss_avg = np.mean(val_losss[var_s_index], axis=0).T
-        plt.plot(np.arange(1, len(loss_avg) + 1), loss_avg, color="b", marker='o', linestyle='--',
-                 label='Training Frame')
-        plt.plot(np.arange(1, len(loss_avg) + 1), val_loss_avg, color="r",
-                 marker='o', linestyle='--', label='Test Frame')
-
-        plt.ylabel('less')
-        plt.yscale('log')
-        plt.xlabel('Training Epoch')
-        plt.legend()
-        plt.grid(which='major', alpha=0.25)
-        plt.xlim([0, params['nEpochs'] + 1])
-        plt.xticks(range(1, params['nEpochs'], 2))
-        plt.savefig(dirname + '/var_si_var_s_db_' + str(var_s) + '_NNconv.pdf', bbox_inches='tight')
+    # for var_s_index, var_s in enumerate(var_si_var_s_db):
+    #     plt.figure()
+    #
+    #     loss_avg = np.mean(losss[var_s_index], axis=0).T
+    #     val_loss_avg = np.mean(val_losss[var_s_index], axis=0).T
+    #     plt.plot(np.arange(1, len(loss_avg) + 1), loss_avg, color="b", marker='o', linestyle='--',
+    #              label='Training Frame')
+    #     plt.plot(np.arange(1, len(loss_avg) + 1), val_loss_avg, color="r",
+    #              marker='o', linestyle='--', label='Test Frame')
+    #
+    #     plt.ylabel('less')
+    #     plt.yscale('log')
+    #     plt.xlabel('Training Epoch')
+    #     plt.legend()
+    #     plt.grid(which='major', alpha=0.25)
+    #     plt.xlim([0, params['nEpochs'] + 1])
+    #     plt.xticks(range(1, params['nEpochs'], 2))
+    #     plt.savefig(dirname + '/var_si_var_s_db_' + str(var_s) + '_NNconv.pdf', bbox_inches='tight')
