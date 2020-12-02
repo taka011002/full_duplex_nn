b6b4d225386ab58e020fce48ca81c7ebb68d9daf
Piece123 <taka.rock.i4@gmail.com>
2020-12-03 00:10:47 +0900

    不要な記述を削除

--- src/nn.py
+++ src/nn.py
@@ -20,11 +20,8 @@ class NNModel:
     error: np.ndarray
 
     def __init__(self, n_hidden: list, learning_rate=0.001, h_si_len: int = 1, h_s_len: int = 1, receive_antenna: int = 1):
-        self.init_model(n_hidden, learning_rate, h_si_len, h_s_len, receive_antenna)
-
-    def init_model(self, n_hidden: list, learning_rate=0.001, h_si_len: int = 1, h_s_len: int = 1, receive_antenna: int = 1):
         input = Input(shape=((2 * h_si_len) + (2 * receive_antenna * h_s_len),))
-        n_hidden = n_hidden.copy() # popだと破壊的操作になり，元々のn_hiddenが壊れるので仕方なくcopyしている
+        n_hidden = n_hidden.copy()  # popだと破壊的操作になり，元々のn_hiddenが壊れるので仕方なくcopyしている
         x = Dense(n_hidden.pop(0), activation='relu')(input)
         for n in n_hidden:
             x = Dense(n, activation='relu')(x)
