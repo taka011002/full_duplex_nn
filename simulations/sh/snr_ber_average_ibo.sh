db108486fb39811fa31ddcc39680baa8986fa3fc
Piece123 <taka.rock.i4@gmail.com>
2020-12-02 21:40:13 +0900

    スクリプト を共通化

--- simulations/sh/snr_ber_average_ibo.sh
+++ simulations/sh/snr_ber_average_ibo.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 SIMULATIONS_NAME="snr_ber_average_ibo"
-CONFIG=`cat $PYTHONPATH/simulations/configs/$SIMULATIONS_NAME.json | jq .  -c`
-NOW=`date "+%Y/%m/%d/%H_%M_%S"`
-OUT="$PYTHONPATH/results/$SIMULATIONS_NAME/$NOW"
-mkdir -p $OUT
-echo "out: $OUT"
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-nohup python $PYTHONPATH/simulations/$SIMULATIONS_NAME.py -c $CONFIG -o $OUT > "$OUT/nohup" &
+sh $SCRIPT_DIR/base.sh $SIMULATIONS_NAME
