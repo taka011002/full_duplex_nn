a751b667037c950447552dbcca2011c7ecd30eb6
Piece123 <taka.rock.i4@gmail.com>
2020-12-03 16:03:13 +0900

    コメントを追加

--- simulations/common/slack.py
+++ simulations/common/slack.py
@@ -2,14 +2,16 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+notify_channel = 'C01FM29EHEF'  # ハードコードしているが，外部から変えられるようにするべき
+
 
 def post_message(message: str):
     client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-    response = client.chat_postMessage(channel='C01FM29EHEF', text=message)
+    response = client.chat_postMessage(channel=notify_channel, text=message)
     return response
 
 
-def upload_file(filepath: str,initial_comment=None):
+def upload_file(filepath: str, initial_comment=None):
     client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-    response = client.files_upload(channels='C01FM29EHEF', file=filepath, initial_comment=initial_comment)
+    response = client.files_upload(channels=notify_channel, file=filepath, initial_comment=initial_comment)
     return response
