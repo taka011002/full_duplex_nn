4386be7d11da6d0f55e3634f86b6b46f5cf5305c
Piece123 <taka.rock.i4@gmail.com>
2020-12-04 14:26:11 +0900

    パスが10本

--- simulations/snr_ber_average_ibo_chain_load.py
+++ simulations/snr_ber_average_ibo_chain_load.py
@@ -36,38 +36,38 @@ if __name__ == '__main__':
     load_files = 1 # 同じ条件で読み込む数
 
     # dirname = settings.dirname_current_datetime(SIMULATIONS_NAME)
-    dirname = "../results/keep/frequency_selective/ch_5_delay_graph"
+    dirname = "../results/keep/frequency_selective/ch_10_delay_graph"
     settings.init_output(dirname)
 
-    param_path = "../results/keep/frequency_selective/ch_5_anthena_1/params.json"
+    param_path = "../results/keep/frequency_selective/ch_10_anthena_1/params.json"
     params = settings.load_param(param_path)
     n_sum = params["test_bits"] * params['SNR_AVERAGE'] * load_files
 
     snrs_db = np.linspace(params['SNR_MIN'], params['SNR_MAX'], params['SNR_NUM'])
     fig, ax = graph.new_snr_ber_canvas(params['SNR_MIN'], params['SNR_MAX'])
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_1/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_1/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 1', 'r')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_1_delay/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_1_delay/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 1(delay)', 'g')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_2/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_2/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 2', 'b')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_2_delay/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_2_delay/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 2(delay)', 'c')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_3/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_3/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 3', 'm')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_3_delay/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_3_delay/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 3(delay)', 'y')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_4/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_4/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 4', 'k')
 
-    pkl_paths = ["../results/keep/frequency_selective/ch_5_anthena_4_delay/result.pkl"]
+    pkl_paths = ["../results/keep/frequency_selective/ch_10_anthena_4_delay/result.pkl"]
     draw_snr_ber(ax, snrs_db, n_sum, pkl_paths, 'receive_antenna: 4(delay)', '#a65628')
 
     ax.legend(fontsize=12)
