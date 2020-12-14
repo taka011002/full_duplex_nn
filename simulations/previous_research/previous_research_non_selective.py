f1c9da2e771eca996df37a5ccd44b913abec1cf0
Piece123 <taka.rock.i4@gmail.com>
2020-12-15 01:49:18 +0900

    0をNoneへ

--- simulations/previous_research/previous_research_non_selective.py
+++ simulations/previous_research/previous_research_non_selective.py
@@ -116,6 +116,7 @@ if __name__ == '__main__':
 
     errors_sum = np.sum(error_array, axis=1)
     bers = errors_sum / n_sum
+    np.place(bers, bers == 0, None)
     ber_ax.plot(snrs_db, bers, color="k", marker='o', linestyle='--', label="Previous research")
     ber_ax.legend()
     plt.savefig(output_dir + '/SNR_BER.pdf', bbox_inches='tight')
