7415f25a01929f7ae2178d54d61bbd0cc5709c91
Takahiro Ishitsuka <taka.rock.i4@gmail.com>
2021-01-08 13:51:45 +0900

    グラフの体裁を修正

--- simulations/common/graph.py
+++ simulations/common/graph.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def init_graph():
     plt.rcParams["font.family"] = "Times New Roman"
-    plt.rcParams["font.size"] = 22
+    plt.rcParams["font.size"] = 26
     plt.rcParams["xtick.direction"] = "in"
     plt.rcParams["ytick.direction"] = "in"
 
@@ -16,12 +17,14 @@ def new_snr_ber_canvas(snr_min: int = 0, snr_max: int = 25, y_min_order: int = -
         plt.Figure, plt.Axes):
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111)
-    ax.set_xlabel("SNR (dB)")
+    ax.set_xlabel("SNR [dB]")
     ax.set_ylabel("BER")
     ax.set_yscale('log')
     ax.set_xlim(snr_min, snr_max)
     y_min = pow(10, y_min_order)
     y_max = pow(10, y_max_order)
+
+    ax.set_xticks(np.arange(snr_min, snr_max+1, 5.0))
     ax.set_ylim(y_min, y_max)
     ax.set_xlim(snr_min, snr_max)
     ax.grid(linestyle='--')
--- simulations/snr_ber_average_ibo_with_previous_chain_load.py
+++ simulations/snr_ber_average_ibo_with_previous_chain_load.py
@@ -43,33 +43,35 @@ if __name__ == '__main__':
     dirname = "../results/keep/snr_ber_average_ibo_with_previous_chain_load"
     settings.init_output(dirname)
 
-    param_path = "../results/snr_ber_average_ibo_with_previous/18_07_00/params.json"
+    param_path = "../results/snr_ber_average_ibo_with_previous/14_26_42/params.json"
     params = settings.load_param(param_path)
     # n_sum = params["test_bits"] * params['SNR_AVERAGE'] * load_files
     n_sum = 3998 * params['SNR_AVERAGE']
 
     snrs_db = np.linspace(params['SNR_MIN'], params['SNR_MAX'], params['SNR_NUM'])
-    fig, ax = graph.new_snr_ber_canvas(params['SNR_MIN'], params['SNR_MAX'], -5)
+    fig, ax = graph.new_snr_ber_canvas(params['SNR_MIN'], params['SNR_MAX'], -3)
 
-    pkl_path = "../results/snr_ber_average_ibo_with_previous/18_07_00/result.pkl"
+    pkl_path = "../results/snr_ber_average_ibo_with_previous/14_26_42/result.pkl"
     # draw_snr_ber(ax, snrs_db, n_sum, pkl_path, 'non lin[7db]', 'k')
     result = load_pkl_file(pkl_path)
 
     errors_sum = np.sum(result.non_cancell_error_array, axis=1)
     bers = errors_sum / (4000 * params['SNR_AVERAGE'])
     np.place(bers, bers == 0, None)
-    ax.plot(snrs_db, bers, color='k', marker='*', linestyle=':', label='w/o canceller')
+    ax.plot(snrs_db, bers, color='k', marker='*', linestyle=':', label='w/o canceller', ms=12)
 
     errors_sum = np.sum(result.error_array, axis=1)
     bers = errors_sum / n_sum
     np.place(bers, bers == 0, None)
-    ax.plot(snrs_db, bers, color='k', marker='x', linestyle='--', label='Conventional [1]')
+    ax.plot(snrs_db, bers, color='k', marker='x', linestyle='--', label='Conventional [1]', ms=12)
 
     errors_sum = np.sum(result.errors, axis=1)
     bers = errors_sum / n_sum
     np.place(bers, bers == 0, None)
-    ax.plot(snrs_db, bers, color='k', marker='o', linestyle='-', label='Proposed')
+    ax.plot(snrs_db, bers, color='k', marker='o', linestyle='-', label='Proposed', ms=12)
 
-    ax.legend(fontsize=24)
-    plt.savefig(dirname + '/SNR_BER.pdf')
-    plt.show()
+    ax.legend(fontsize=24, loc=3)
+    plt.savefig(dirname + '/SNR_BER.eps', bbox_inches='tight')
+    plt.savefig(dirname + '/SNR_BER.pdf', bbox_inches='tight')
+
+    # plt.show()
