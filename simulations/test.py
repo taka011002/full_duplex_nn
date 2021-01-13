9cd499a277e170ffb0dc36c6d16b81fd3a75b3ba
Takahiro Ishitsuka <taka.rock.i4@gmail.com>
2021-01-13 14:18:28 +0900

    ofdmの実装を検証

--- simulations/test.py
+++ simulations/test.py
@@ -1,6 +1,7 @@
 from src import modules as m
 import numpy as np
 from src import ofdm
+from scipy.linalg import dft
 
 #
 # c = m.channel(1, 2, 1)
@@ -12,14 +13,49 @@ from src import ofdm
 
 # 送信信号
 # n = 100
-N = 32 # サブキャリア数
-P = 3 # CP長
+N = 6 # サブキャリア数
+P = 2 # CP長
+M = 1 # 通信路長
 
 # s = np.random.choice([0, 1], n * N)
 # x = m.modulate_qpsk(s)
 
-s = np.random.choice([0, 1], 32 * 2)
-x = m.modulate_qpsk(s)
-x_idft = ofdm.IDFT(x)
+F = dft(N)
+FH = F.conj().T
 
-print("end")
+# h_si = m.channel(1, M).reshape(M)
+s = np.random.choice([0, 1], (N * 2, 1))
+x = m.modulate_qpsk(s)
+x = x * np.sqrt(2) # 電力制限を一旦なくす
+# x_idft = ofdm.IDFT(x)
+x_idft = FH @ x
+x_cp = ofdm.add_cp(x_idft, P)
+# r = np.convolve(h_si, x_cp)
+# r_remove_cp = ofdm.remove_cp(r, P, N)
+# s = ofdm.DFT(r_remove_cp)
+
+h_si = m.channel(1, M+1)
+# h_si = np.array([1 + 1j, 1 + 1j]).reshape((1, 2))
+
+H = ofdm.toeplitz_channel(h_si.T, M, N, P)
+delay = np.zeros((M, 1))
+x_hat = np.vstack((delay, x_cp))
+r = H @ x_hat
+# r = H[:, 1:] @ x_cp
+Rcp = np.hstack((np.zeros((N, P)), np.eye(N, N)))
+r_remove_cp = Rcp @ r
+
+# Hcを作って計算する
+Hc = ofdm.Hc(h_si.T, M, N)
+r_perfect = Hc @ x_idft
+y = F @ r_remove_cp
+D = F @ Hc @ FH
+D_1 = np.linalg.inv(D)
+x_hat = D_1 @ y
+s_hat = m.demodulate_qpsk(x_hat.squeeze()).reshape((N * 2, 1))
+error = np.sum(s != s_hat)
+# p_error = np.sum(s != s_hat)
+
+
+print(error)
+# print(p_error)
--- src/ofdm.py
+++ src/ofdm.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from scipy.linalg import toeplitz
+from scipy.linalg import circulant
 
 def IDFT(data):
     """
@@ -9,3 +10,35 @@ def IDFT(data):
     :return:
     """
     return np.fft.ifft(data)
+
+
+def DFT(data):
+    return np.fft.fft(data)
+
+
+def add_cp(data, cp_len):
+    cp = data[-cp_len:]
+    return np.vstack((cp, data))
+
+
+def remove_cp(data, cp_len, data_len):
+    return data[cp_len:(cp_len + data_len)]
+
+
+def toeplitz_channel(h: np.ndarray, h_len: int, subcarrier, cp) -> np.ndarray:
+    H_row = np.zeros((h_len + subcarrier + cp, 1), dtype=complex)
+    H_row[:h_len+1] = h
+    H_col = np.zeros((subcarrier + cp, 1), dtype=complex)
+    H_col[0] = h[0]
+    H = toeplitz(H_col, r=H_row)
+    return H
+
+def Hc(h: np.ndarray, h_len: int, subcarrier) -> np.ndarray:
+    # H_row = np.zeros((subcarrier, 1), dtype=complex)
+    # H_row[subcarrier-(h_len):] = np.flipud(h)[:h_len]
+    # H_row[0] = h[0]
+    H_col = np.zeros((subcarrier, 1), dtype=complex)
+    H_col[:h_len+1] = h
+    # H = toeplitz(H_col, H_row)
+    H = circulant(H_col)
+    return H
